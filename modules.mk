b2971468751a70934dcba42bbe63e757acba074d
Donghyeon Lee <earthicko@gmail.com>
2023-01-10 19:02:16 +0900

    Chore: 루트 Makefile에서 모듈 관련 변수를 modules.mk로 분리

---
+++ modules.mk
@@ -0,0 +1,76 @@
+################################# LIBRARIES ####################################
+LIB_DIR				= modules
+
+LINK_READLINE		= -L${HOME}/.brew/opt/readline/lib -lreadline
+
+LIBFT_DIR			= $(LIB_DIR)/libft
+LIBFT				= $(LIBFT_DIR)/libft.a
+LINK_LIBFT			= -L$(LIBFT_DIR)
+INC_DIR_LIBFT		= -I$(LIBFT_DIR)/includes
+
+LIBLEXER_DIR		= $(LIB_DIR)/liblexer
+LIBLEXER			= $(LIBLEXER_DIR)/liblexer.a
+LINK_LIBLEXER		= -L$(LIBLEXER_DIR)
+INC_DIR_LIBLEXER	= -I$(LIBLEXER_DIR)/includes
+
+LIBPARSER_DIR		= $(LIB_DIR)/libparser
+LIBPARSER			= $(LIBPARSER_DIR)/libparser.a
+LINK_LIBPARSER		= -L$(LIBPARSER_DIR)
+INC_DIR_LIBPARSER	= -I$(LIBPARSER_DIR)/includes
+
+LIBENVMAN_DIR		= $(LIB_DIR)/libenvman
+LIBENVMAN			= $(LIBENVMAN_DIR)/libenvman.a
+LINK_LIBENVMAN		= -L$(LIBENVMAN_DIR)
+INC_DIR_LIBENVMAN	= -I$(LIBENVMAN_DIR)/includes
+
+LIBBUILTIN_DIR		= $(LIB_DIR)/libbuiltin
+LIBBUILTIN			= $(LIBBUILTIN_DIR)/libbuiltin.a
+LINK_LIBBUILTIN		= -L$(LIBBUILTIN_DIR)
+INC_DIR_LIBBUILTIN	= -I$(LIBBUILTIN_DIR)/includes
+
+LIB_ALL				= \
+					$(LIBFT) \
+					$(LIBLEXER) \
+					$(LIBPARSER) \
+					$(LIBENVMAN) \
+					$(LIBBUILTIN)
+LINK_LIBS			= \
+					$(LINK_READLINE) \
+					$(LINK_LIBFT) \
+					$(LINK_LIBLEXER) \
+					$(LINK_LIBPARSER) \
+					$(LINK_LIBENVMAN) \
+					$(LINK_LIBBUILTIN)
+INC_DIR				= -I. -Iincludes \
+					$(INC_DIR_LIBFT) \
+					$(INC_DIR_LIBLEXER) \
+					$(INC_DIR_LIBPARSER) \
+					$(INC_DIR_LIBENVMAN) \
+					$(INC_DIR_LIBBUILTIN)
+################################# TARGETS ######################################
+include $(LIBFT_DIR)/filelist.mk
+include $(LIBLEXER_DIR)/filelist.mk
+include $(LIBPARSER_DIR)/filelist.mk
+include $(LIBENVMAN_DIR)/filelist.mk
+include $(LIBBUILTIN_DIR)/filelist.mk
+
+ABS_SRC_LIBFT = $(addprefix $(LIBFT_DIR)/, $(SRC_LIBFT))
+ABS_SRC_LIBLEXER = $(addprefix $(LIBLEXER_DIR)/, $(SRC_LIBLEXER))
+ABS_SRC_LIBPARSER = $(addprefix $(LIBPARSER_DIR)/, $(SRC_LIBPARSER))
+ABS_SRC_LIBENVMAN = $(addprefix $(LIBENVMAN_DIR)/, $(SRC_LIBENVMAN))
+ABS_SRC_LIBBUILTIN = $(addprefix $(LIBBUILTIN_DIR)/, $(SRC_LIBBUILTIN))
+
+$(LIBFT): $(ABS_SRC_LIBFT)
+	@make -j4 -C $(LIBFT_DIR)/
+
+$(LIBLEXER): $(ABS_SRC_LIBLEXER)
+	@make -j4 -C $(LIBLEXER_DIR)/
+
+$(LIBPARSER): $(ABS_SRC_LIBPARSER)
+	@make -j4 -C $(LIBPARSER_DIR)/
+
+$(LIBENVMAN): $(ABS_SRC_LIBENVMAN)
+	@make -j4 -C $(LIBENVMAN_DIR)/
+
+$(LIBBUILTIN): $(ABS_SRC_LIBBUILTIN)
+	@make -j4 -C $(LIBBUILTIN_DIR)/
