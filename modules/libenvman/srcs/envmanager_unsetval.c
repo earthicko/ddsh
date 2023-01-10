ae66d92d10566e7e4ebb386bfe89447c1d7f5bd8
Donghyeon Lee <earthicko@gmail.com>
2023-01-10 20:23:57 +0900

    Feat: envmanager_unsetval가 엔트리를 찾지 못할 시 에러 코드 반환

--- modules/libenvman/srcs/envmanager_unsetval.c
+++ modules/libenvman/srcs/envmanager_unsetval.c
@@ -6,7 +6,8 @@ int	envmanager_unsetval(t_list **envlist, char *name)
 	t_list	*list;
 
 	list = find_list_with_entry(*envlist, name);
-	if (list)
-		ft_lstunlink(envlist, list, free_entry);
+	if (!list)
+		return (CODE_ERROR_DATA);
+	ft_lstunlink(envlist, list, free_entry);
 	return (CODE_OK);
 }
