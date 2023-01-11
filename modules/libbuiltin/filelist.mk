5acbd201d226c872cf58aabb0058270f7e1dc93e
Donghyeon Lee <earthicko@gmail.com>
2023-01-11 18:25:19 +0900

    Fix: builtin Makefile에 파일 추가

--- modules/libbuiltin/filelist.mk
+++ modules/libbuiltin/filelist.mk
@@ -4,7 +4,9 @@ FILE_LIBBUILTIN	= \
 					builtin_echo \
 					builtin_export \
 					builtin_pwd \
-					builtin_unset
+					builtin_unset \
+					builtin_env \
+					builtin_exit
 
 SRC_LIBBUILTIN	= $(addprefix srcs/, $(addsuffix .c, $(FILE_LIBBUILTIN)))
 OBJ_LIBBUILTIN	= $(addprefix srcs/, $(addsuffix .o, $(FILE_LIBBUILTIN)))
