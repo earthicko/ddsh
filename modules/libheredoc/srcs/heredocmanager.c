6d46a5c9b214f469bbd1fb4d09cd3caadce17444
Donghyeon Lee <earthicko@gmail.com>
2023-01-11 20:34:30 +0900

    Feat: heredocmanager 파일 이름 생성

--- modules/libheredoc/srcs/heredocmanager.c
+++ modules/libheredoc/srcs/heredocmanager.c
@@ -1,10 +1,53 @@
+#include <stdlib.h>
 #include <unistd.h>
+#include <readline/readline.h>
 #include "libft.h"
 #include "heredoc_internal.h"
 
+static char	*extract_filename(char *fullname)
+{
+	char	*cursor;
+
+	cursor = fullname;
+	while (*cursor)
+		cursor++;
+	while (cursor != fullname)
+	{
+		if (*cursor == '/')
+			break ;
+		cursor--;
+	}
+	if (*cursor == '/')
+		cursor++;
+	return (ft_strdup(cursor));
+}
+
+static char	*generate_filename(void)
+{
+	int		slot;
+	char	*fullname;
+	char	*filename;
+
+	slot = ttyslot();
+	if (slot < 0)
+		return (NULL);
+	fullname = ttyname(slot);
+	if (!fullname)
+		return (NULL);
+	filename = extract_filename(fullname);
+	if (!filename)
+		return (NULL);
+	return (ft_strjoin(PREFIX_HEREDOC_TEMPFILE, filename));
+}
+
 int	heredoc_create(int *n_heredoc)
 {
+	char	*filename;
+
 	(void)n_heredoc;
+	filename = generate_filename();
+	ft_printf("filename %s\n", filename);
+	free(filename);
 	return (ft_printf("Unimplemented stub of heredoc_create.\n"));
 }
 
