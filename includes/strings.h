dcf0b63946c87f3eebb0f67e361bc21208afbda8
Donghyeon Lee <earthicko@gmail.com>
2023-01-11 18:04:47 +0900

    Feat: main() 추가

---
+++ includes/strings.h
@@ -0,0 +1,5 @@
+#ifndef STRINGS_H
+# define STRINGS_H
+# define MSG_SHELL_PROMPT "dsh$ "
+
+#endif
--- srcs/prompt/minishell.c
+++ srcs/prompt/minishell.c
@@ -1,39 +1,38 @@
+#include <stdlib.h>
+#include <unistd.h>
 #include <readline/readline.h>
-#include "libft_def.h"
+#include "libft.h"
 #include "lexer.h"
 #include "parser.h"
 #include "t_node.h"
-
-void	print_tok_arr(t_toks *toks)
-{
-	int	i;
-
-	i = -1;
-	if (!toks)
-	{
-		printf("toks is null\n\n");
-		return ;
-	}
-	while (++i < toks->n_toks)
-	{
-		printf("type: %d\n", toks->arr[i].type);
-		printf("arr[%d]: %s\n", i, toks->arr[i].content);
-	}
-	printf("\n");
-}
+#include "strings.h"
+#include "testers.h"
 
 int	main(void)
 {
 	char	*str;
 	t_toks	toks;
 	t_node	*exec_tree;
+	int		stat;
 
-	while (1)
+	while (TRUE)
 	{
-		str = readline("minishell: ");
-		if (lexer(str, &toks) == CODE_OK)
-			exec_tree = parse_tokens(&toks->arr, &toks->n_toks);
+		str = readline(MSG_SHELL_PROMPT);
+		if (!str)
+			continue ;
+		if (ft_strlen(str) == 0)
+			continue ;
+		add_history(str);
+		stat = lexer(str, &toks);
+		free(str);
+		if (stat)
+			continue ;
+		exec_tree = parse_tokens(toks.arr, toks.n_toks);
+		// toks_destroy(&toks);
 		if (exec_tree)
-			excute_cmd(exec_tree);
+			node_print(exec_tree, 0);
+		node_destroy(exec_tree);
+		system("leaks minishell");
 	}
+	return (CODE_OK);
 }
