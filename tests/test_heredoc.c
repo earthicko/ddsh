2bcccbbf07084fd9dc504169df5f921329ee94df
Donghyeon Lee <earthicko@gmail.com>
2023-01-13 22:28:29 +0900

    Test: heredoc 테스트 보완

--- tests/test_heredoc.c
+++ tests/test_heredoc.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include "heredoc.h"
+#include "envmanager.h"
 
-int	main(void)
+int	main(int argc, char **argv, char **envp)
 {
 	char	*filename;
+	char	*delim1;
+	char	*delim2;
 
+	(void)argc;
+	(void)argv;
+	envmanager(envp, NULL, NULL, NULL);
 	heredocmanager(HEREDOCMODE_CLEAR, -1, NULL);
-	heredocmanager(HEREDOCMODE_READ, 0, "EOF");
-	heredocmanager(HEREDOCMODE_READ, 0, "EOF");
+	delim1 = strdup("EO\"F");
+	delim2 = strdup("EOF");
+	heredocmanager(HEREDOCMODE_READ, 0, delim1);
+	heredocmanager(HEREDOCMODE_READ, 0, delim2);
+	free(delim1);
+	free(delim2);
 	heredocmanager(HEREDOCMODE_GETFILENAME, 0, &filename);
 	printf("filename: %s\n", filename);
 	free(filename);
