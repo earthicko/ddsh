5ede54222acd56e5074e12ee1a0195c9ba278f46
Donghyeon Lee <earthicko@gmail.com>
2023-01-13 23:53:51 +0900

    Feat: envman_split_envstr에 올바른 Name 체크 기능 추가

--- modules/libenvman/srcs/envmanager_utils.c
+++ modules/libenvman/srcs/envmanager_utils.c
@@ -22,24 +22,44 @@ char	*envman_compose_envstr(char *name, char *val)
 	return (temp);
 }
 
+static size_t	find_equal_sign(char *str, size_t len)
+{
+	size_t	i_split;
+
+	i_split = 0;
+	while (i_split < len)
+	{
+		if (str[i_split] == ENVSTR_DELIM_CHAR)
+			return (i_split);
+		i_split++;
+	}
+}
+
+static int	split_envstr_abort(char **name, char **val, int stat)
+{
+	if (*name)
+		free(*name);
+	if (*val)
+		free(*val);
+	*name = NULL;
+	*val = NULL;
+	return (stat);
+}
+
 int	envman_split_envstr(char *str, char **ret_name, char **ret_val)
 {
 	size_t	i_split;
 	size_t	len_str;
 
 	len_str = ft_strlen(str);
-	i_split = 0;
-	while (i_split < len_str)
-	{
-		if (str[i_split] == ENVSTR_DELIM_CHAR)
-			break ;
-		i_split++;
-	}
+	i_split = find_equal_sign(str, len_str);
 	if (i_split == len_str)
 		return (CODE_ERROR_DATA);
 	*ret_name = ft_substr(str, 0, i_split);
 	*ret_val = ft_substr(str, i_split + 1, len_str - i_split - 1);
 	if (!(*ret_name && *ret_val))
-		return (CODE_ERROR_MALLOC);
+		return (split_envstr_abort(ret_name, ret_val, CODE_ERROR_MALLOC));
+	if (!is_valid_name(*ret_name))
+		return (split_envstr_abort(ret_name, ret_val, CODE_ERROR_DATA));
 	return (CODE_OK);
 }
