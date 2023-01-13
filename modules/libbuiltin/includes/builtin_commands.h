6c4ef0ebf6e9569189f21ad3fca7fc12bf77d347
Donghyeon Lee <earthicko@gmail.com>
2023-01-14 00:58:58 +0900

    Feat: builtin 커맨드 헤더 추가

---
+++ modules/libbuiltin/includes/builtin_commands.h
@@ -0,0 +1,12 @@
+#ifndef BUILTIN_COMMANDS_H
+# define BUILTIN_COMMANDS_H
+
+int	builtin_echo(char **argv);
+int	builtin_cd(char **argv);
+int	builtin_pwd(void);
+int	builtin_export(char **argv);
+int	builtin_unset(char **argv);
+int	builtin_env(char **argv);
+int	builtin_exit(char **argv);
+
+#endif
