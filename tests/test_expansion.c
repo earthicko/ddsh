943b6d28a71d63349d00422e25576091ce6c8c4b
Donghyeon Lee <earthicko@gmail.com>
2023-01-14 01:35:08 +0900

    Test: test_expansion 보완 (find_exec 테스트)

--- tests/test_expansion.c
+++ tests/test_expansion.c
@@ -2,10 +2,12 @@
 #include <stdio.h>
 #include <string.h>
 #include "envmanager.h"
+#include "strutils.h"
 
 int	main(int argc, char **argv, char **envp)
 {
 	char	*word;
+	char	*execname;
 
 	(void)argc;
 	(void)argv;
@@ -15,6 +17,14 @@ int	main(int argc, char **argv, char **envp)
 	printf("exit status %d\n", envmanager_replace_envvar(&word, 0));
 	printf("after: <%s>\n", word);
 	free(word);
+	execname = strdup("brew");
+	printf("before: <%s>\n", execname);
+	find_exec(&execname);
+	printf("after: <%s>\n", execname);
+	free(execname);
+	execname = strdup("export");
+	printf("exit status %d\n", find_exec(&execname));
+	free(execname);
 	system("leaks test_expansion");
 	return (0);
 }
