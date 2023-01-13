05e0114aee2b96158f670176e8f36b7d2b1e1b29
Donghyeon Lee <earthicko@gmail.com>
2023-01-13 23:31:29 +0900

    Feat: 빌트인 cd에 OLDPWD, PWD 설정 루틴 추가

--- modules/libbuiltin/srcs/builtin_cd.c
+++ modules/libbuiltin/srcs/builtin_cd.c
@@ -1,8 +1,26 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
 #include "libft.h"
 #include "envmanager.h"
 #include "builtin_internal.h"
+
+static int	builtin_cd_internal(char *target)
+{
+	char	*pwd;
+	int		stat;
+
+	pwd = getcwd(NULL, 0);
+	if (pwd)
+		envmanager(NULL, NULL, ENVVAR_OLDPWD, pwd);
+	free(pwd);
+	stat = chdir(target);
+	pwd = getcwd(NULL, 0);
+	if (pwd)
+		envmanager(NULL, NULL, ENVVAR_PWD, pwd);
+	free(pwd);
+	return (stat);
+}
 
 int	builtin_cd(char **argv)
 {
@@ -12,17 +30,18 @@ int	builtin_cd(char **argv)
 	if (!ft_strncmp(argv[0], FLAG_OLDPWD, ft_strlen(FLAG_OLDPWD)))
 	{
 		if (envmanager(NULL, &target, ENVVAR_OLDPWD, NULL))
+		{
+			printf("Unimplemented error message of cd.\n");
 			return (1);
-		stat = chdir(target);
-		free(target);
-		if (stat)
-			return (1);
+		}
 	}
 	else
 	{
-		stat = chdir(argv[0]);
-		if (stat)
+		target = ft_strdup(argv[0]);
+		if (!target)
 			return (1);
 	}
-	return (0);
+	stat = builtin_cd_internal(target);
+	free(target);
+	return (stat);
 }
