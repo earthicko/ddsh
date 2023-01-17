7b6a3819826158d025dc249e1f573a41d6b8fed5
Donghyeon Lee <earthicko@gmail.com>
2023-01-17 16:54:55 +0900

    Fix: exit_stat_manager가 256 이상의 수를 저장할수 있도록

--- modules/libenvman/srcs/exit_stat_manager.c
+++ modules/libenvman/srcs/exit_stat_manager.c
@@ -1,12 +1,12 @@
 // set new_stat to negative to retreive prev exit stat.
-// set new stat to 0 <= new_stat < 256 to set exit stat.
+// set new stat to 0 <= new_stat to set exit stat.
 int	exit_stat_manager(int new_stat)
 {
 	static int	stat;
 
 	if (new_stat < 0)
 		return (stat);
-	if (0 <= new_stat && new_stat < 256)
+	if (0 <= new_stat)
 	{
 		stat = new_stat;
 		return (0);
