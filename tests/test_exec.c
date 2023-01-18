31bf8d5809871476a3c258955c6a82ffdde8a58a
dakang <trumpjqk@daum.net>
2023-01-18 21:27:55 +0900

    Test: test_exec 수정, bzero 추가

--- tests/test_exec.c
+++ tests/test_exec.c
@@ -9,10 +9,16 @@
 #include "parser.h"
 #include "build_exec.h"
 #include "executor.h"
+#include "envmanager.h"
+#include "heredoc.h"
 
 int		get_n_redir(t_node *node);
 void	node_print(t_node *root, int depth);
-char	**g_envp;
+
+static void	test_get_n_redir(char *str, t_toks *toks)__attribute__((unused));
+static void	test_build_unit(char *str)__attribute__((unused));
+static void	test_exec(char *input)__attribute__((unused));
+
 
 static char	*get_redir_typename(int type)
 {
@@ -126,48 +132,31 @@ static void	test_get_n_redir(char *str, t_toks *toks)
 
 static void	test_exec(char *input)
 {
-/*
-<<<<<<< HEAD
-	t_toks	toks;
-	t_node	*root;
-	t_unit_arr units;
-	int			status;
-
-	status = 100;
-	
-	dprintf(2, "input: %s\n\n", input);
-	if (lexer(input, &toks) != CODE_OK
-			|| !(root = parse_tokens(toks.arr, toks.n_toks))
-			|| build_exec_unit(root, &units) != CODE_OK
-			|| (status = executor(&units)) != CODE_OK)
-		dprintf(2, "\nin %s, Some error occured\n", __func__);
-	//현재 err_io 발생중... 어디지 ㅇㅁㅇ?
-	dprintf(2, "in %s, status code: %d\n", __func__, status);
-	dprintf(2, "\n>==============<\n\n\n");
-=======
-*/
 	t_toks		toks;
 	t_node		*root;
 	t_unit_arr	units;
 	int			status;
 
-	status = 100;
+	status = 42;
 	dprintf(2, "input: %s\n\n", input);
 	if (lexer(input, &toks) != CODE_OK)
-		dprintf(2, "Some error occur\n");
+		dprintf(2, "Failed while lexing\n");
 	root = parse_tokens(toks.arr, toks.n_toks);
 	if (!root)
-		dprintf(2, "Some error occur\n");
-	if (build_exec_unit(root, &units) != CODE_OK)
-		dprintf(2, "Some error occur\n");
-	if ((status = executor(&units)) != CODE_OK)
-		dprintf(2, "Some error occur\n");
+		dprintf(2, "Failed while parsing\n");
+	if (root && build_exec_unit(root, &units) != CODE_OK)
+		dprintf(2, "Failed while building exec_units\n");
+	if (root && (status = executor(&units)) != CODE_OK)
+		dprintf(2, "Failed while executing\n");
 	dprintf(2, "in %s, status code: %d\n", __func__, status);
 	dprintf(2, "\n>============================<\n\n\n");
+	toks.arr = 0;
+	toks.n_toks = 0;
 }
 
-void	set_command_1(char **command)
+void	set_command_1(char *command[20])
 {
+	ft_bzero(command, 160);
 	command[0] = "a";
 	//command[0] = "<a <b cat -e file >a > c >d";
 	command[1] = "cat <a file -e file <a >b <<b";
@@ -176,8 +165,9 @@ void	set_command_1(char **command)
 	command[4] = "cat <a >b <<b | cat -e > file";
 }
 
-void	set_command_2(char **command)
+void	set_command_2(char *command[20])
 {
+	ft_bzero(command, 160);
 	command[0] = "a";
 	//command[0] = "<a <b cat -e file >a > c >d";
 	command[1] = "<a <b cat -e | file >a > c >d";
@@ -186,93 +176,70 @@ void	set_command_2(char **command)
 	command[4] = "a";
 }
 
-void	set_command_3(char **command)
+void	set_command_3(char *command[20])
 {
-	command[0] = "<README.md cat | cat | cat | cat";
-	//command[0] = "cat README.md -e";
+	ft_bzero(command, 160);
+	/********basic test***********/
+	command[0] = "cat README.md -e";
 	command[1] = "cat -e README.md";
-	command[2] = "ls -l | cat";
+	command[2] = "ls -l | cat | cat";
 	command[3] = "cat | no_cmd";
-	command[4] = "yes you | cat | no_cmd";
-	command[5] = "cat Makefile | cat | cat";
-	command[6] = "cat Makefile | head -n 5 | cat | tail -n 5";
-	command[7] = "yes you | cat | cat | head -n 5";
-	command[8] = "echo -e hi | cat | ls | cat -e";
-//>>>>>>> refs/remotes/origin/main
+	command[4] = "cat Makefile | head -n 5 | cat | tail -n 5";
+	command[5] = "echo -e hi | cat | ls | cat -e";
+	/*******uncloosed pipe test*********/
+	command[6] = "yes you | cat | no_cmd";
+	command[7] = "yes you | ls | cat";
+	command[8] = "yes you | cat | cat | head -n 5";
+	command[9] = "cat </dev/urandom | cat | cat | head -n 5";
+	/*******redirection test***********/
+	command[10] = "echo hi > a > b | cat | no_cmd";
+	command[11] = "<README.md cat > hi";
+	command[12] = "cat README.md > a > b>c | ls | cat c";
+	command[13] = "echo append >> c";
 }
 
-//테스트케이스 반복문으로 작성하면 훨씬 깔끔한듯(다른사람이 볼 때)
-//
-//다만 반복문으로 작성했을 때, 특정 줄만 테스트 돌리기가 쉽지 않음.
-//
-//또 wgcc때문에 모든 함수는 최소 한 번이상 호출해야함(메이크파일 고쳐서 해결되긴 할듯)
-//그래서 각 테스트 함수를 최소한 한번 호출하기 위해서는 출력을 최소화하는 함수 호출이 필요
-//(결국 특정한 인풋을 처리하는게 필요함, 0번 인덱스에 해당 인풋을 넣어도 되긴 할듯)
 int	main(int argc, char *argv[], char *envp[])
 {
 	t_toks	toks;
-	char	*commands[10];
+	char	*commands[20];
 	int		i;
+	int		stat;
 
 	(void)argc;
 	(void)argv;
-	g_envp = envp;
+	(void)envp;
+	
 	set_command_1(commands);
 	printf(">=========TEST GET_N_REDIR<=========\n");
 	i = -1;
 	while (++i < 0)
 		test_get_n_redir(commands[i], &toks);
-	//test_get_n_redir("", &toks);
-	test_get_n_redir(0, &toks);
-	toks.arr = 0;
 	printf("\n>=========================<\n\n\n");
+
+
 	printf(">=========TEST GET_EXEC_UNIT<=========\n");
 	set_command_2(commands);
 	i = -1;
-	while (++i < 1)
+	while (++i < 0)
 		test_build_unit(commands[i]);
+	printf("\n>=========================<\n\n\n");
+
+
 	printf("\n\n>=========TEST EXECTUION<=========\n\n\n");
-
-/**********************************/
-//<<<<<<< HEAD
-	/********basic test***********/
-	/*
-	test_exec("cat README.md -e");
-	test_exec("cat -e README.md");
-	test_exec("ls -l | cat | cat");
-	test_exec("cat | no_cmd");
-	*/
-
-	/*******uncloosed pipe test*********/
-	/*
-	test_exec("yes you | cat | no_cmd");
-	test_exec("yes you | ls | cat");
-	test_exec("cat README.md | cat | cat");
-	test_exec("cat README.md | head -n 5 | cat | tail -n 5");
-	test_exec("yes you | cat | cat | head -n 5");
-	test_exec("echo -e hi | cat | ls | cat -e");
-	*/
-
-	/******redirection test*********/
-	//test_exec("echo hi > a > b | cat | no_cmd");
-	//test_exec("<README.md cat > hi");
-	//test_exec("echo hi > a > b > c | ls | cat c");
-	//test_exec("echo append >> c");
-	//위 테스트 케이스 세그폴트 발생
-	//
-	//system("leaks test_exec");
-//=======
-/**********************************/
-
+	stat = envman_init(envp);
+	if (stat)
+		return (1);
+	//stat = heredoc_init();
+	if (stat)
+		return (1);
 	set_command_3(commands);
 	i = -1;
-	while (++i < 9)
+	while (++i < 0)
 		test_exec(commands[i]);
-	//test_exec("echo append >> c");
+	test_exec("cat");
+	//test_exec("cat | cat | cat | ls");
 	//아래 테케는 아직 동작하지 않음
 	//test_exec("<< EOF cat");
-	//system("lsof -p");
 	//system("leaks test_exec");
 	return (0);
-//>>>>>>> refs/remotes/origin/main
 }
