3a79f96077bc6478d7a5199e2f57b1518db5c314
dakang <trumpjqk@daum.net>
2023-01-20 00:05:24 +0900

    Refactor & Comment: 자식프로세스 실행로직 변경, TODO 추가 - 자식프로스에서 먼저 빌트인 명령어인지 체크후 이후 단계로 진행 (find_exec에서 빌트인 명령어 체크하는 로직 삭제) - 에러메시지 및 exit status관련 TODO 추가 -child_exec_cmd 놈에 맞게 함수 분할

--- modules/libexec/srcs/executor_child_exec.c
+++ modules/libexec/srcs/executor_child_exec.c
@@ -9,6 +9,7 @@
 #include "strutils.h"
 
 
+//TODO: 시그널 함수 추가
 //int	signal_set_state_default(void);
 
 static int	set_fd_stream(t_info *info)
@@ -40,9 +41,57 @@ static int	set_fd_stream(t_info *info)
 	return (process_redir(cur_unit.redir_arr, cur_unit.n_redir));
 }
 
+void	if_dir_then_exit_126(char *cmd_name)
+{
+	struct stat	s_statbuf;
+
+	if (stat(cmd_name, &s_statbuf) != 0)
+	{
+		perror("stat function failed\n");
+		exit(EXIT_FAILURE);
+	}
+	if (S_ISDIR(s_statbuf.st_mode))
+	{
+		dprintf(2, "shell: %s: is a directory\n", cmd_name);
+		exit(126);
+	}
+}
+
+void	child_exec_extern(t_info *info)
+{
+	char	**argv;
+	int		status;
+	char	**envp_paths;
+
+	argv = (info->units->arr + info->cur_idx)->argv;
+	status = envman_getenvp(&envp_paths);
+	if (status)
+	{
+		// TODO: 에러메시지를 상위루틴(이 함수)에서 출력하는 것이 나아보임
+		ft_printf("%s: error while getenvp, stat %d\n", __func__, status);
+		exit(EXIT_FAILURE);
+	}
+	status = find_exec(&argv[0]);
+	// TODO: 아래 두 경우에 대한 에러메시지는 서브루틴에서 처리
+	if (status == CODE_ERROR_MALLOC)
+		exit(EXIT_FAILURE);
+	if (status == CODE_ERROR_GENERIC)
+		exit(127);
+	if_dir_then_exit_126(argv[0]);	
+	if (access(argv[0], X_OK) == 0)
+		execve(argv[0], argv, envp_paths);
+	else 
+	{
+		dprintf(2, "shell: %s: Permission denied\n", argv[0]);
+		exit(126);
+	}
+	//As the convention, call _exit(127) in such case
+	exit(127);
+}
+
 // 참고만: stat, lstat 심볼릭 링크파일인 경우 동작이 다르대..
 // TODO: 빌트인 커맨드 실패에 대한 exit status 세분화
-// TODO: 놈 맞추기, 함수 쪼개기
+//
  void	child_exec_cmd(t_info *info)
 {
 	struct stat	s_statbuf;
@@ -53,44 +102,16 @@ static int	set_fd_stream(t_info *info)
 
 	if (set_fd_stream(info) < 0)
 		exit(EXIT_FAILURE);
-	argv = info->units->arr[info->cur_idx].argv;
-	status = envman_getenvp(&envp_paths);
-	if (status)
+	argv = (info->units->arr + info->cur_idx)->argv;
+	status = is_builtin_command(argv[0]);
+	if (status != FALSE)
 	{
-		// 에러메시지를 여기서 체크해도 무방할듯
-		ft_printf("%s: error while getenvp, stat %d\n", __func__, status);
-		exit(EXIT_FAILURE);
-	}
-	status = find_exec(&argv[0]);
-	if (status > 0)
-	{
+		// TODO: 빌트인함수 에러메시지 출력은 서브루틴에 추가
+		// CODE_OK가 아닌 상황에서 exit status 세분화 필요
 		builtin_stat = exec_builtin_cmd(info->units->arr + info->cur_idx, SUBSHELL);
 		if (builtin_stat != CODE_OK)
 			exit(EXIT_FAILURE);
 		exit(CODE_OK);
 	}
-	// 아래 두 경우에 대한 에러메시지는 서브루틴에서 처리
-	if (status == CODE_ERROR_MALLOC)
-		exit(EXIT_FAILURE);
-	if (status == CODE_ERROR_GENERIC)
-		exit(127);
-	if (stat(argv[0], &s_statbuf) != 0)
-	{
-		perror("stat function failed\n");
-		exit(EXIT_FAILURE);
-	}
-	if (S_ISDIR(s_statbuf.st_mode))
-	{
-		dprintf(2, "shell: %s: is a directory\n", argv[0]);
-		exit(126);
-	}
-	if (access(argv[0], X_OK) == 0)
-		execve(argv[0], argv, envp_paths);
-	else 
-	{
-		dprintf(2, "shell: %s: Permission denied\n", argv[0]);
-		exit(126);
-	}
-	//As the convention, call _exit(127) in such case
-	exit(127);
+	child_exec_extern(info);
 }
