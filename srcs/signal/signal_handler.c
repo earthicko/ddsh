5ffa078b90c8b25e66d138902197a5ca02c67819
Donghyeon Lee <earthicko@gmail.com>
2023-01-20 18:59:04 +0900

    Feat: signal 핸들러 완성

--- srcs/signal/signal_handler.c
+++ srcs/signal/signal_handler.c
@@ -2,10 +2,12 @@
 #include <stdio.h>
 #include <readline/readline.h>
 #include "libft.h"
+#include "msgdef.h"
 
 void	_sighandler_int_interactive(int signo)
 {
-	ft_printf("%s: got SIGINT(%d)\n", __func__, signo);
+	(void)signo;
+	ft_printf("\r%s%s  \n", MSG_SHELL_PROMPT, rl_line_buffer);
 	rl_replace_line("", TRUE);
 	rl_on_new_line();
 	rl_redisplay();
@@ -14,7 +16,7 @@ void	_sighandler_int_interactive(int signo)
 
 void	_sighandler_int_heredoc(int signo)
 {
-	ft_printf("%s: got SIGINT(%d)\n", __func__, signo);
+	(void)signo;
 	exit(0);
 	return ;
 }
