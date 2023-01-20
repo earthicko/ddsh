e02d9a28a3cd73b4b62b4dd7b48374459e2adf3f
Donghyeon Lee <earthicko@gmail.com>
2023-01-20 22:28:28 +0900

    Feat: builtin env 구현

--- modules/libbuiltin/srcs/builtin_env.c
+++ modules/libbuiltin/srcs/builtin_env.c
@@ -1,7 +1,113 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include "libft.h"
+#include "strutils.h"
+#include "msgdef.h"
+#include "envmanager.h"
+
+int	builtin_export(char **argv);
+
+static int	_builtin_env_print(void)
+{
+	char	**envp;
+	char	**cursor;
+
+	if (envman_getenvp(&envp))
+		return (1);
+	cursor = envp;
+	while (*cursor)
+	{
+		if (ft_printf("%s\n", *cursor) < 0)
+		{
+			ft_free_strarr(envp);
+			return (1);
+		}
+		cursor++;
+	}
+	ft_free_strarr(envp);
+	return (0);
+}
+
+static void	_if_dir_then_exit_126(char *cmd_name)
+{
+	struct stat	s_statbuf;
+
+	if (stat(cmd_name, &s_statbuf) != 0)
+	{
+		ft_dprintf(2, "%s: env: %s: failed to fetch stat\n",
+			MSG_ERROR_PREFIX, cmd_name);
+		exit(1);
+	}
+	if (S_ISDIR(s_statbuf.st_mode))
+	{
+		ft_dprintf(2, "%s: env: %s: is a directory\n",
+			MSG_ERROR_PREFIX, cmd_name);
+		exit(126);
+	}
+}
+
+static void	_builtin_env_exec(char **argv)
+{
+	char	**envp;
+
+	if (envman_getenvp(&envp))
+		exit(1);
+	_if_dir_then_exit_126(argv[0]);
+	if (access(argv[0], X_OK) == 0)
+		execve(argv[0], argv, envp);
+	else
+	{
+		ft_dprintf(2, "%s: env: %s: Permission denied\n",
+			MSG_ERROR_PREFIX, argv[0]);
+		exit(126);
+	}
+	exit(127);
+}
+
+static void	_builtin_env_child(char **argv)
+{
+	char	*argv_to_export[3];
+	char	*pair[2];
+	int		stat;
+
+	argv_to_export[0] = "export";
+	argv_to_export[2] = NULL;
+	while (*argv)
+	{
+		if (envman_split_envstr(*argv, &(pair[0]), &(pair[1])))
+			break ;
+		free(pair[0]);
+		free(pair[1]);
+		argv_to_export[1] = *argv;
+		if (builtin_export(argv_to_export))
+			break ;
+		argv++;
+	}
+	stat = find_exec(&argv[0]);
+	if (stat && stat == CODE_ERROR_GENERIC)
+		exit(127);
+	if (stat)
+		exit(1);
+	_builtin_env_exec(argv);
+	exit(127);
+}
 
 int	builtin_env(char **argv)
 {
-	(void)argv;
-	return (printf("Unimplemented stub of builtin env.\n"));
+	pid_t	pid;
+	int		stat;
+
+	argv++;
+	if (!(*argv))
+		return (_builtin_env_print());
+	pid = fork();
+	if (pid == -1)
+		return (1);
+	if (pid)
+		wait4(pid, &stat, 0, NULL);
+	else
+		_builtin_env_child(argv);
+	return (WEXITSTATUS(stat));
 }
