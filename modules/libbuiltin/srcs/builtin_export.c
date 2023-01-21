da11c4bb55dc17755a213dee2572ff220a453ef3
Donghyeon Lee <earthicko@gmail.com>
2023-01-21 17:24:55 +0900

    Fix: export에 =이 없는 인자가 들어갈 시 조건 분기
    - =가 없고 올바른 이름이면 정상 종료
    - =가 없고 틀린 이름이면 에러 처리

--- modules/libbuiltin/srcs/builtin_export.c
+++ modules/libbuiltin/srcs/builtin_export.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include "libft.h"
+#include "strutils.h"
 #include "msgdef.h"
 #include "envmanager.h"
 
@@ -32,22 +33,29 @@ static int	export_display(void)
 	return (0);
 }
 
+static int	_exit_export_var(int stat, char *word)
+{
+	if (stat == CODE_ERROR_DATA)
+		ft_dprintf(2, "%s: export: `%s': not a valid identifier\n",
+			MSG_ERROR_PREFIX, word);
+	else if (stat == CODE_ERROR_MALLOC)
+		ft_print_error(MSG_ERROR_PREFIX, CODE_ERROR_MALLOC);
+	return (1);
+}
+
 static int	export_var(char *word)
 {
 	char	*name;
 	char	*val;
 	int		stat;
 
+	if (!ft_strchr(word, '=') && !is_valid_name(word))
+		return (_exit_export_var(CODE_ERROR_DATA, word));
+	if (!ft_strchr(word, '='))
+		return (0);
 	stat = envman_split_envstr(word, &name, &val);
 	if (stat)
-	{
-		if (stat == CODE_ERROR_DATA)
-			ft_dprintf(2, "%s: export: `%s': not a valid identifier\n",
-				MSG_ERROR_PREFIX, word);
-		else if (stat == CODE_ERROR_MALLOC)
-			ft_print_error(MSG_ERROR_PREFIX, CODE_ERROR_MALLOC);
-		return (1);
-	}
+		return (_exit_export_var(stat, word));
 	stat = envman_setval(name, val);
 	free(name);
 	free(val);
