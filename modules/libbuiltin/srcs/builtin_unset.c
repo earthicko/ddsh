7e6b1a2e477e28b9ecc700190905eac305a266d7
Donghyeon Lee <earthicko@gmail.com>
2023-01-22 15:22:02 +0900

    Feat: unset에 valid한 name인지 확인 로직 추가

--- modules/libbuiltin/srcs/builtin_unset.c
+++ modules/libbuiltin/srcs/builtin_unset.c
@@ -1,11 +1,18 @@
+#include "libft.h"
 #include "envmanager.h"
+#include "msgdef.h"
+#include "strutils.h"
 
 int	builtin_unset(char **argv)
 {
 	argv++;
 	while (*argv)
 	{
-		envman_unsetval(*argv);
+		if (is_valid_name(*argv))
+			envman_unsetval(*argv);
+		else
+			ft_dprintf(2, "%s: unset: `%s': not a valid identifier\n",
+				MSG_ERROR_PREFIX, *argv);
 		argv++;
 	}
 	return (0);
