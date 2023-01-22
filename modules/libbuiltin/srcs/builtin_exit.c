ebf39c7bede7bd5d1b8b40a1e68a35c7184122fa
geneeol <trumpjqk@daum.net>
2023-01-22 15:39:38 +0900

    Fix: exit 인자개수 2개 이상인 경우에 대한 에러 분기 추가

--- modules/libbuiltin/srcs/builtin_exit.c
+++ modules/libbuiltin/srcs/builtin_exit.c
@@ -1,19 +1,25 @@
 #include <stdlib.h>
 #include "libft.h"
 #include "envmanager.h"
+#include "msgdef.h"
 
 int	builtin_exit(char **argv)
 {
 	int	stat;
 	int	ret;
 
+	if (argv[1] && argv[2])
+	{
+		ft_dprintf(2, "%s: exit: too many arguments\n", MSG_ERROR_PREFIX);
+		return (1);
+	}
 	if (argv[1])
 	{
 		ret = ft_atoi_if_valid(argv[1], &stat);
 		if (ret)
 		{
 			ft_dprintf(2, "%s: numeric argument required\n", argv[1]);
-			stat = 255;
+			stat = 2;
 		}
 		exit(stat);
 	}
