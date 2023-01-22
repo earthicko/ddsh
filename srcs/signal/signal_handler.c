afa8f006b82ca768955bcd171659d32a404a512e
geneeol <trumpjqk@daum.net>
2023-01-22 20:07:55 +0900

    Fix: ctrl + c 입력관련 에러 수정

--- srcs/signal/signal_handler.c
+++ srcs/signal/signal_handler.c
@@ -6,7 +6,7 @@
 /*   By: dahkang <dahkang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 16:43:47 by dahkang           #+#    #+#             */
-/*   Updated: 2023/01/22 16:43:52 by dahkang          ###   ########.fr       */
+/*   Updated: 2023/01/22 20:00:57 by dahkang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 void	_sighandler_int_interactive(int signo)
 {
 	(void)signo;
-	ft_printf("\r%s  \n", MSG_SHELL_PROMPT);
+	ft_printf("\r%s%s  \n", MSG_SHELL_PROMPT, rl_line_buffer);
 	rl_replace_line("", TRUE);
 	rl_on_new_line();
 	rl_redisplay();
