997124e46ff884f00ba934a48f892f5dfd2274c5
Donghyeon Lee <earthicko@gmail.com>
2023-01-24 18:39:09 +0900

    Feat: helper script 추가

---
+++ leaks.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+while :
+do
+	leaks minishell
+	sleep 1
+done
+
---
+++ norm.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+norminette includes modules/libbuiltin modules/libenvman modules/libexec modules/libft modules/libheredoc modules/liblexer modules/libparser srcs
+echo $?
