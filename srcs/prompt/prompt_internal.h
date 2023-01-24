e61c4b82708ef259c81e6d9d28f78184068330f5
Donghyeon Lee <earthicko@gmail.com>
2023-01-24 16:05:01 +0900

    Refactor: prompt_init->prompt_clear

--- srcs/prompt/prompt_internal.h
+++ srcs/prompt/prompt_internal.h
@@ -16,7 +16,7 @@
 # define PROMPT_INTERNAL_H
 # define CODE_CONTINUE 1
 
-int	prompt_init(void);
+int	prompt_clear(void);
 int	prompt_getstr(char **buf);
 int	prompt_gettokens(char *str, t_toks *toks);
 int	prompt_getparsetree(t_toks *toks, t_node **buf);
