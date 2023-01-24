2ed2e7c3299d4be1d5ee1346a4336c7a526a7fa6
Donghyeon Lee <earthicko@gmail.com>
2023-01-24 16:03:54 +0900

    Feat: execute_line() 추가

--- srcs/prompt/prompt_main.c
+++ srcs/prompt/prompt_main.c
@@ -18,12 +18,27 @@
 #include "t_node.h"
 #include "prompt_internal.h"
 
+void	execute_line(char *line)
+{
+	int		stat;
+	t_toks	toks;
+	t_node	*parse_tree;
+
+	stat = prompt_gettokens(line, &toks);
+	if (stat)
+		return ;
+	stat = prompt_getparsetree(&toks, &parse_tree);
+	token_destroy(&toks);
+	if (stat)
+		return ;
+	prompt_execute(parse_tree);
+	node_destroy(parse_tree);
+	return ;
+}
+
 int	main(int argc, char **argv, char **envp)
 {
-	char		*str;
-	t_toks		toks;
-	t_node		*parse_tree;
-	int			stat;
+	char	*line;
 
 	(void)argc;
 	(void)argv;
@@ -31,18 +46,10 @@ int	main(int argc, char **argv, char **envp)
 	heredoc_init();
 	while (TRUE)
 	{
-		if (prompt_init() || prompt_getstr(&str))
+		if (prompt_clear() || prompt_getstr(&line))
 			continue ;
-		stat = prompt_gettokens(str, &toks);
-		free(str);
-		if (stat)
-			continue ;
-		stat = prompt_getparsetree(&toks, &parse_tree);
-		token_destroy(&toks);
-		if (stat)
-			continue ;
-		prompt_execute(parse_tree);
-		node_destroy(parse_tree);
+		execute_line(line);
+		free(line);
 	}
-	return (CODE_OK);
+	return (exit_stat_manager(-1));
 }
