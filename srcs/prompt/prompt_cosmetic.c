88748d83e6c651dafa3cfe9ec30267356a1e6833
earth <earthicko@gmail.com>
2023-01-26 01:55:06 +0900

    Feat: 프롬프트에 색 추가

--- srcs/prompt/prompt_cosmetic.c
+++ srcs/prompt/prompt_cosmetic.c
@@ -15,16 +15,16 @@
 
 void	prompt_print_banner(void)
 {
-	ft_printf("%s%s%s\n", _B_00_0, _B_00_1, _B_00_2);
-	ft_printf("%s%s%s\n", _B_01_0, _B_01_1, _B_01_2);
-	ft_printf("%s%s%s\n", _B_02_0, _B_02_1, _B_02_2);
-	ft_printf("%s%s%s\n", _B_03_0, _B_03_1, _B_03_2);
-	ft_printf("%s%s%s\n", _B_04_0, _B_04_1, _B_04_2);
-	ft_printf("%s%s%s\n", _B_05_0, _B_05_1, _B_05_2);
-	ft_printf("%s%s%s\n", _B_06_0, _B_06_1, _B_06_2);
-	ft_printf("%s%s%s\n", _B_07_0, _B_07_1, _B_07_2);
-	ft_printf("%s%s%s\n", _B_08_0, _B_08_1, _B_08_2);
-	ft_printf("%s%s%s\n", _B_09_0, _B_09_1, _B_09_2);
-	ft_printf("%s%s%s%s%s %s\n\n", _B_10_0, _B_10_1, _B_10_2,
+	ft_printf("%s%s%s%s\n", _B_00_0, _B_00_1, _B_00_2, _B_00_3);
+	ft_printf("%s%s%s%s\n", _B_01_0, _B_01_1, _B_01_2, _B_01_3);
+	ft_printf("%s%s%s%s\n", _B_02_0, _B_02_1, _B_02_2, _B_02_3);
+	ft_printf("%s%s%s%s\n", _B_03_0, _B_03_1, _B_03_2, _B_03_3);
+	ft_printf("%s%s%s%s\n", _B_04_0, _B_04_1, _B_04_2, _B_04_3);
+	ft_printf("%s%s%s%s\n", _B_05_0, _B_05_1, _B_05_2, _B_05_3);
+	ft_printf("%s%s%s%s\n", _B_06_0, _B_06_1, _B_06_2, _B_06_3);
+	ft_printf("%s%s%s%s\n", _B_07_0, _B_07_1, _B_07_2, _B_07_3);
+	ft_printf("%s%s%s%s\n", _B_08_0, _B_08_1, _B_08_2, _B_08_3);
+	ft_printf("%s%s%s%s\n", _B_09_0, _B_09_1, _B_09_2, _B_09_3);
+	ft_printf("%s%s%s%s%s%s %s\n\n", _B_10_0, _B_10_1, _B_10_2, _B_10_3,
 		_B_CREDIT_0, _B_CREDIT_1, _B_VER);
 }
