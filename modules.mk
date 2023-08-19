92e8ff474411f268f1da6e15816b2ee22fd0a1fa
Donghyeon Lee <earthicko@gmail.com>
2023-08-19 11:06:06 +0900

    Feat: add Termux environment support

--- modules.mk
+++ modules.mk
@@ -1,10 +1,17 @@
 LIB_DIR				= ${PWD}/modules
 
 ifeq ($(OSNAME),Linux)
-	LIBREADLINE_DIR		= /usr/lib/x86_64-linux-gnu
-	LIBREADLINE			= $(LIBREADLINE_DIR)/libreadline.a
-	LINK_LIBREADLINE	= -L$(LIBREADLINE_DIR) -lreadline
-	INC_DIR_LIBREADLINE	= -I/usr/include
+	ifeq ($(shell [ -d "/data/data/com.termux/files/usr" ] && echo 1), 1)
+		LIBREADLINE_DIR		= /data/data/com.termux/files/usr/lib
+		LIBREADLINE			= $(LIBREADLINE_DIR)/libreadline.so
+		LINK_LIBREADLINE	= -L$(LIBREADLINE_DIR) -lreadline
+		INC_DIR_LIBREADLINE	= -I/data/data/com.termux/files/usr/include
+	else
+		LIBREADLINE_DIR		= /usr/lib/x86_64-linux-gnu
+		LIBREADLINE			= $(LIBREADLINE_DIR)/libreadline.a
+		LINK_LIBREADLINE	= -L$(LIBREADLINE_DIR) -lreadline
+		INC_DIR_LIBREADLINE	= -I/usr/include
+	endif
 else ifeq ($(OSNAME),Darwin)
 	LIBREADLINE_DIR		= $(LIB_DIR)/readline
 	LIBREADLINE			= $(LIBREADLINE_DIR)/lib/libreadline.a
